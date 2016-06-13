70bcb9f9bb2db0a2632bb61563ac0ca940c7190a
Alex <garridoalex77@gmail.com>
2016-06-12 23:07:15 -0500

    created migrations for users, answers, questions

---
+++ app/database/migrations/2016_06_13_022608_Create_Users_Table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email')->unique();
+			$table->string('username');
+			$table->string('password');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('pref_lang')->nullable();
+			$table->integer('rating')->nullable();
+			$table->integer('answers_count')->nullable();
+			$table->string('is_admin');
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
