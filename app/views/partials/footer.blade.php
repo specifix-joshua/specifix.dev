bfe97d46fce558df3a77870f5c6dd112fe688922
Karina <karina@alisons-MacBook-Air.local>
2016-06-13 11:02:32 -0500

    Made a simple footer based on the mockup. Added comments where the content needs to be changed depending on what is agreed upon.

---
+++ app/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+    <div class="container one">
+        <h3>Specifix</h3>
+        <!--link to an about us page-->
+        <!--copyright?-->
+    </div>
+
+    <div class="container two">
+        <h3>Connect Socially</h3>
+        <!--Social media icons?-->
+    </div>
+
+    <div class="container three">
+        <h3>Contact Information</h3>
+        <ul>
+            <li>Company Name and Address</li>
+            <li>Phone Number</li>
+            <li>E-mail</li>
+            <li>Message an Admin</li>
+        </ul>
+    </div>
+</footer>
