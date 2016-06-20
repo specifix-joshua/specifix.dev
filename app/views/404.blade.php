9cb862e1abbec8535c81e3c289773d33ebdda77e
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-06-19 23:00:10 -0500

    Added 404 page

---
+++ app/views/404.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+
+@section('title-tag')
+    Your Questions, Answered.
+@stop
+
+@section('top-links')
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+@stop
+
+@section('content')
+<div id='home-hero' class="container-fluid">
+	<div class="container">
+		<div id='blog-jumbo' class="jumbotron">
+			<h1><span class='roboto-slab roboto-bold'>THIS IS NOT A PAGE, BRO.</h1>
+			<p>404 Error. Try again...</p>
+		</div>
+	</div>
+</div>
+<div class="container">
+@stop
