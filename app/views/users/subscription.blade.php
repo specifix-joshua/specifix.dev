23022feca0f5b32e345b4c2bef6881c149c49c10
Karina <karina@alisons-MacBook-Air.local>
2016-06-21 15:35:25 -0500

    Added an extra div that a user will only see if they cancelled a subscription. It will contain a link to resume a cancelled subscrption.

--- app/views/users/subscription.blade.php
+++ app/views/users/subscription.blade.php
@@ -12,23 +12,38 @@
             <div class="caption">
                 <h2>Monthly Subscription</h2>
                 <h3>$4.99/month</h3>
-                <p>Want to improve your specifix experience? How about signing up for a premium membership, where you will have access to expert knowledge, and your questions will always be featured at the top of the list. Try it out!</p>
-                {{Form::open(array('action' => 'UsersController@createSubscription')) }}
-                  <script
-                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                    data-key="pk_test_q0cXMhVKaYM4JB8Y8ao1mU3o"
-                    data-amount="499"
-                    data-name="Specifix"
-                    data-description="Monthly Subscription"
-                    data-image="/img/documentation/checkout/marketplace.png"
-                    data-locale="auto"
-                    data-zip-code="true"
-                  </script>
-
-                {{ Form::close() }}
+                <p>Want to improve your specifix experience? How about signing up for a premium membership, where you will have access to expert knowledge, and your questions will always be featured in the premium question page. Try it out!</p>
+                @if(Auth::check() && Auth::user() && Auth::id() == Auth::user()->id && !Auth::user()->subscribed())
+                        {{Form::open(array('action' => 'UsersController@createSubscription')) }}
+                          <script
+                            src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                            data-key="pk_test_q0cXMhVKaYM4JB8Y8ao1mU3o"
+                            data-amount="499"
+                            data-name="Specifix"
+                            data-description="Monthly Subscription"
+                            data-image="/img/specifix-bounty.png"
+                            data-locale="auto"
+                            data-zip-code="true"
+                          </script>
+                        {{ Form::close() }}
+                    @else
+                    <p><small>You are already subscribed to this service!</small></p>
+                @endif
             </div>
         </div>
     </div>
+
+    @if(Auth::check() && Auth::user() && Auth::id() == Auth::user()->id && Auth::user()->cancelled())
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="thumbnail">
+                <div class="caption">
+                <h2>Resume Subscription</h2>
+                <p>If you have cancelled your subscription, you have the option to resume it. Don't worry, you will not be charged right away if you are currently on a grace period. We will wait until the next billing cycle.</p>
+                <p><a href="{{{ action('UsersController@resumeSubscription', Auth::user()->id) }}}">Resume Subscription</a>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
 
 @stop
