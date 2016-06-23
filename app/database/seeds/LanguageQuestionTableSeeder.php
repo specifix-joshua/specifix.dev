1edb7e86fe7fdbc7b745e416f44d2fc679c02c0e
Alex <garridoalex77@gmail.com>
2016-06-23 14:31:02 -0500

    added seeds to pivot tables

--- app/database/seeds/LanguageQuestionTableSeeder.php
+++ app/database/seeds/LanguageQuestionTableSeeder.php
@@ -26,6 +26,11 @@ class LanguageQuestionTableSeeder extends Seeder {
                     'language_id' => 2,
                     ]
                 ];
-         DB::table('language_question')->insert($data);
+
+        for ($i=4; $i<25; $i++) {
+           $data[] = ['question_id' => $i, 'language_id' => mt_rand(1, 3),];
+           $data[] = ['question_id' => $i, 'language_id' => mt_rand(4, 7),];
+        };
+        DB::table('language_question')->insert($data);
     }
 }
